af566d6e94a9f737386e8f1af9af348827b58ebb
ismail <ismailessoudaigui.com>
2023-10-11 10:08:06 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,20 +5,17 @@
  */
 void print_alphabet_x10(void)
 {
-    char letter = 'a';
-    int count = 0;
-
-    while (count < 10)
-    {
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        letter = 'a';  // Reset the letter to 'a' for the next iteration
-        count++;
-    }
-
-    _putchar('\n');
+char letter;
+int count;
+for (count = 0; count < 10; count++)
+{
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+}
+_putchar('\n');
 }
 
