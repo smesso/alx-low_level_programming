3eb680ec660f4bdd6118abbcb0d27c8aff60fd71
ismail <ismailessoudaigui.com>
2023-10-16 18:59:01 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Update the value pointed to by the pointer to 98
+ * @n: Pointer to an integer
+ */
+void reset_to_98(int *n)
+{
+if (n != NULL)
+{
+*n = 98;
+}
+}
