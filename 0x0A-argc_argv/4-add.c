663c71f7f1353a9e4388636a5083c233ab5564b7
ismail <ismailessoudaigui.com>
2023-11-01 15:30:59 -0700

    updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - Sums up numbers passed as command-line arguments.
- * @argc: Number of arguments.
- * @argv: Array of arguments.
+ * main - program that adds positive numbers.
+ * @argc: number of arguments
+ * @argv: array with the arguments
  *
- * Return: 0 if successful, 1 if there are non-integer arguments, or 0 if no arguments provided.
+ * Return: (0) on success, (1) on error
  **/
-int main(int argc, char *argv[]) 
+
+int main(int argc, char *argv[])
 {
-    int sum = 0;
-if (argc == 1) 
+int i, sum = 0;
+char c[] = "Error";
+
+if (argc > 1)
 {
-        printf("0\n");
-        return (0);
-    }
-    
-    for (int i = 1; i < argc; i++) 
+for (i = 1; i < argc; i++)
 {
-        char *num = argv[i];
-        
-        for (int j = 0; num[j] != '\0'; j++) 
-	{
-            if (!isdigit(num[j])) 
-	    {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        
-        sum += atoi(num);
-    }
-    
-    printf("%d\n", sum);
-    
-    return (0);
+char *find_letter = argv[i];
+
+while (*find_letter != '\0')
+{
+if (*find_letter < '0' || *find_letter > '9')
+{
+printf("%s\n", c);
+return (1);
+}
+find_letter++;
+}
+
+int num = atoi(argv[i]);
+sum += num;
+}
+
+printf("%d\n", sum);
+}
+else
+{
+printf("%d\n", 0);
+}
+
+return (0);
 }
