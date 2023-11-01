0302775edd6f02ce34d737571b522a69f8ef48fe
ismail <ismailessoudaigui.com>
2023-10-31 17:51:14 -0700

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,24 +11,27 @@
  *
  * Return: (Success) a pointer to the concatenated string; (Failure) NULL.
  */
-char *str_concat(char *s1, char *s2)
-{
-if (s1 == NULL)
-{
+char *str_concat(char *s1, char *s2) {
+int len1, len2;
+char *result;
+
+if (s1 == NULL) {
 s1 = "";
 }
-if (s2 == NULL)
-{
+if (s2 == NULL) {
 s2 = "";
 }
-int len1 = strlen(s1);
-int len2 = strlen(s2);
-char *result = malloc(len1 + len2 + 1);
-if (result == NULL)
-{
+
+len1 = strlen(s1);
+len2 = strlen(s2);
+
+result = malloc(len1 + len2 + 1);
+if (result == NULL) {
 return (NULL);
 }
+
 strcpy(result, s1);
 strcat(result, s2);
+
 return (result);
 }
