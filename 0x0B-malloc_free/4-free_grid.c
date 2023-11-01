b8e0256fc75f1fe985880934004965ee22fbc06c
ismail <ismailessoudaigui.com>
2023-11-01 12:27:40 -0700

    updated

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,14 +11,16 @@
  */
 void free_grid(int **grid, int height)
 {
+int i;
 if (grid == NULL || height <= 0)
 {
 return;
 }
-int i;
+
 for (i = 0; i < height; i++)
 {
 free(grid[i]);
 }
+
 free(grid);
 }
