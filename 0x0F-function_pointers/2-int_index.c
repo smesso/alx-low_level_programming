965303e9921ec73c1d78f16f2fab714f7a4efd04
ismail <ismailessoudaigui.com>
2023-11-08 16:11:11 -0800

    updated

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,11 +9,14 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-if (array && cmp)
+if (size <= 0)
 {
+return (-1);
+}
 for (int i = 0; i < size; i++)
 {
-if (cmp(array[i]))
+if (cmp(array[i]) != 0)
+{
 return (i);
 }
 }
