3c4e5e936ae926dbc04c6b8d8f0c80306ef6f4b5
ismail <ismailessoudaigui.com>
2023-11-11 13:30:57 -0800

    updated

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * int_index - return index place if comparison = true, else -1
@@ -9,16 +11,19 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-if (size <= 0)
-{
+int i;
+
+if (array == NULL)
 return (-1);
-}
-for (int i = 0; i < size; i++)
+if (size <= 0)
+return (-1);
+if (cmp == NULL)
+return (-1);
+
+for (i = 0; i < size; i++)
 {
 if (cmp(array[i]) != 0)
-{
 return (i);
-}
 }
 return (-1);
 }
