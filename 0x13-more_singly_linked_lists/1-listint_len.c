ea4c7c146daa35bf6bfa944aec227b1d1cf33f78
ismail <ismailessoudaigui.com>
2023-11-21 16:29:06 -0800

    updated

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
