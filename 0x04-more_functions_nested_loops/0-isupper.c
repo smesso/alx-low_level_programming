29bc12607cbf2511b0f6ba209975196657fbc33a
Ismail <ismailessoudaigui@gmail.com>
2024-02-26 10:11:34 -0800

    0x04

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,19 +3,15 @@
 /**
  * _isupper - checks for lowercase character.
  * @c: caracter recibo por main.
- * Return: 1 if c is uppercase
+ * Return: 1 if c is uppercase and 0 if it isnt
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
-	}
-	else if (c <= 'z')
-	{
-		return (0);
-	}
-	else
+	} else
 	{
 		return (0);
 	}
