634e58714cb67b0f3c00134958ef3318608ec3fb
Ismail <ismailessoudaigui@gmail.com>
2024-02-26 10:26:09 -0800

    0x04

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,7 +17,7 @@ void more_numbers(void)
 		for (number = 0; number <= 14; number++)
 		{
 			tens = number / 10;
-			units = number % 10;			
+			units = number % 10;		
 			if (number > 9)
 				_putchar(tens + '0');
 			_putchar(units + '0');
